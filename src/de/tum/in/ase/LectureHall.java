60198f3c4d16f900334f47f7a04f009a68d0049d
MetehanOner <a.metehanoner@gmail.com>
2022-12-03 01:28:20 +0100

    Init project

---
+++ src/de/tum/in/ase/LectureHall.java
@@ -0,0 +1,7 @@
+package de.tum.in.ase;
+
+public class LectureHall {
+
+
+
+}
---
+++ src/de/tum/in/ase/Participant.java
@@ -0,0 +1,43 @@
+package de.tum.in.ase;
+
+public abstract class Participant {
+    private String firstName;
+    private String lastName;
+    private String tumID;
+
+    protected Participant(String firstName, String lastName, String tumID){
+        setFirstName(firstName);
+        setLastName(lastName);
+        setTumID(tumID);
+    }
+
+    public String getName(){
+        return getFirstName() + getLastName();
+    }
+
+    public abstract void attend();
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getTumID() {
+        return tumID;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setTumID(String tumID) {
+        this.tumID = tumID;
+    }
+}
