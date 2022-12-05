0b404c408a321c2f3e20cc77cd609020d4562d27
MetehanOner <a.metehanoner@gmail.com>
2022-12-05 21:33:14 +0100

    tutor logic+++

--- src/de/tum/in/ase/ExerciseInstructor.java
+++ src/de/tum/in/ase/ExerciseInstructor.java
@@ -16,13 +16,20 @@ public class ExerciseInstructor extends Participant {
     @Override
     public void attend(Lecture lecture) {
 
-        if(lecture.getExerciseInstructors().contains(this) == this.lecture.getExerciseInstructors().contains(this)){
+        //if(lecture.getExerciseInstructors().contains(this)){
+
+            int index = this.lecture.getExerciseInstructors().indexOf(this);
             int indices = lecture.getExerciseInstructors().indexOf(this);
-            System.out.println(lecture.getExerciseInstructors().get(indices).getName() +" tumID:"+ lecture.getExerciseInstructors().get(indices).getTumID() +" already attends/holds the lecture.");
-        } else {
-            Student s = new Student(this.getFirstName(), this.getLastName(), this.getTumID());
-            lecture.addParticipant(s);
-        }
+
+            if(this.lecture.getExerciseInstructors().get(index) == lecture.getExerciseInstructors().get(indices)){
+                System.out.println(lecture.getExerciseInstructors().get(indices).getName() +" tumID:"+ lecture.getExerciseInstructors().get(indices).getTumID() +" already attends/holds the lecture.");
+            } else {
+                Student s = new Student(this.getFirstName(), this.getLastName(), this.getTumID());
+                lecture.addParticipant(s);
+            }
+
+        //}
+
 
         /*if(lecture.getExerciseInstructors().contains(this)){
             int indicators = lecture.getExerciseInstructors().indexOf(this);
--- src/de/tum/in/ase/Tutor.java
+++ src/de/tum/in/ase/Tutor.java
@@ -14,13 +14,23 @@ public class Tutor extends Participant{
     @Override
     public void attend(Lecture lecture) {
 
-        if(lecture.getTutors().contains(this) == this.lecture.getTutors().contains(this)){
-            int indices = lecture.getTutors().indexOf(this);
+        int index = this.lecture.getTutors().indexOf(this);
+        int indices = lecture.getTutors().indexOf(this);
+
+        if(this.lecture.getTutors().get(index) == lecture.getTutors().get(indices)){
             System.out.println(lecture.getTutors().get(indices).getName() +" tumID:"+ lecture.getTutors().get(indices).getTumID() +" already attends/holds the lecture.");
         } else {
             Student s = new Student(this.getFirstName(), this.getLastName(), this.getTumID());
             lecture.addParticipant(s);
         }
+
+        /*if(lecture.getTutors().contains(this) == this.lecture.getTutors().contains(this)){
+            int indices = lecture.getTutors().indexOf(this);
+            System.out.println(lecture.getTutors().get(indices).getName() +" tumID:"+ lecture.getTutors().get(indices).getTumID() +" already attends/holds the lecture.");
+        } else {
+            Student s = new Student(this.getFirstName(), this.getLastName(), this.getTumID());
+            lecture.addParticipant(s);
+        }*/
 
         /*
         if(lecture.getTutors().contains(this.lecture.getTutors().)){
