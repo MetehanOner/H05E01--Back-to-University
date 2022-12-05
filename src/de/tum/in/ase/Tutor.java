ed63e5d1a41dee72a9181a6a7109a85eb8734058
MetehanOner <a.metehanoner@gmail.com>
2022-12-05 21:49:06 +0100

    tutor logic+++ test+++ test

--- src/de/tum/in/ase/Tutor.java
+++ src/de/tum/in/ase/Tutor.java
@@ -14,39 +14,12 @@ public class Tutor extends Participant{
     @Override
     public void attend(Lecture lecture) {
 
-        if(lecture.getTutors().contains(this)){
-
-            int index = this.lecture.getTutors().indexOf(this);
-            int indices = lecture.getTutors().indexOf(this);
-
-            if(this.lecture.getTutors().get(index) == lecture.getTutors().get(indices)){
-                System.out.println(lecture.getExerciseInstructors().get(indices).getName() +" tumID:"+ lecture.getExerciseInstructors().get(indices).getTumID() +" already attends/holds the lecture.");
-            } else {
-                Student s = new Student(this.getFirstName(), this.getLastName(), this.getTumID());
-                lecture.addParticipant(s);
-            }
-
+        if(this.lecture.getTutors().contains(this)){
+            System.out.println( " tumID:"+ "" +" already attends/holds the lecture.");
         } else {
             Student s = new Student(this.getFirstName(), this.getLastName(), this.getTumID());
             lecture.addParticipant(s);
         }
-
-        /*if(lecture.getTutors().contains(this) == this.lecture.getTutors().contains(this)){
-            int indices = lecture.getTutors().indexOf(this);
-            System.out.println(lecture.getTutors().get(indices).getName() +" tumID:"+ lecture.getTutors().get(indices).getTumID() +" already attends/holds the lecture.");
-        } else {
-            Student s = new Student(this.getFirstName(), this.getLastName(), this.getTumID());
-            lecture.addParticipant(s);
-        }*/
-
-        /*
-        if(lecture.getTutors().contains(this.lecture.getTutors().)){
-            int indices = lecture.getTutors().indexOf(this);
-            System.out.println(lecture.getTutors().get(indices).getName() +" tumID:"+ lecture.getTutors().get(indices).getTumID() +" already attends/holds the lecture.");
-        } else {
-            Student s = new Student(this.getFirstName(), this.getLastName(), this.getTumID());
-            lecture.addParticipant(s);
-        }*/
 
     }
 
